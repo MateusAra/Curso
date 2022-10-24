b51fa3e7e2b100bc6939bae438eda29f96ef74a0
Mateus de Sousa Araújo <mateus.araujo@fitbank.com.br>
2022-10-24 09:54:46 -0300

    first commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress"/>
+
+it('successfully logs in', () => {
+    cy.login(
+        Cypress.env('USER_EMAIL'),
+        Cypress.env('USER_PASSWORD'),
+        { cacheSession: false}
+    )
+})
