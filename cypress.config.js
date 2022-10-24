de42e953b7d5ff346744c58d202af55524861bcb
Mateus de Sousa Araújo <mateus.araujo@fitbank.com.br>
2022-10-24 12:25:11 -0300

    Tamanho padrão da viewPort

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,10 @@ module.exports = defineConfig({
     baseUrl: "https://notes-serverless-app.com",
     chromeWebSecurity: false,
     experimentalSessionAndOrigin: true,
+    experimentalStudio: true,
+    "env": {
+      "viewportWidthBreakpoint": 768
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
