a57c53fc04bcab6c1112e9eba99308f6a3e2cd7a
Mateus Araújo <96430863+MateusAra@users.noreply.github.com>
2023-05-10 20:35:47 -0300

    Update login.spec.cy.js

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,9 +1,11 @@
 /// <reference types="cypress"/>
 
 it('successfully logs in', () => {
+    
     cy.login(
         Cypress.env('USER_EMAIL'),
         Cypress.env('USER_PASSWORD'),
         { cacheSession: false}
     )
+    
 })
